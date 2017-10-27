009ad50f0ca85c4f5b9b77d9d70c08f05a508b71
pirushanp <pirushanp@gmail.com>
2017-10-27 08:17:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ function chocolateBars (){
   var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
   return chocolateBars
 }
+function addElementToBeginningOfArray(array, element) {
+  const array = [1]
+  const addsanelementtothebeginningofanarray = [...'foo', array]
+}
